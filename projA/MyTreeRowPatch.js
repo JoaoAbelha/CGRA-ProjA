f0e2b0f9e001aba4f23e1cc4aacd1448065c5ec4
JoAPSo Rafael Gomes Varela <up201706072@feup-b313d03.fe.up.pt>
2019-03-25 10:40:35 +0000

    Added random displacements on rowpatches

--- projA/MyTreeRowPatch.js
+++ projA/MyTreeRowPatch.js
@@ -7,18 +7,25 @@ class MyTreeRowPatch extends CGFobject {
     constructor(scene) {
         super(scene);
         this.tree = new MyTree(scene, 6, 1,1,1);
+    
+        this.displacementsOnX = [];
+        for(let i = 0; i < 6; i++) {
+            this.displacementsOnX.push(Math.floor(Math.random() * 2)).toFixed(4);
+        }
 
+        this.displacementsOnZ = [];
+        for(let j = 0; j < 6; j++) {
+            this.displacementsOnZ.push(Math.floor(Math.random() * 2)).toFixed(4);
+        }
     };
     display() {
         const side = 6;
 
         for(let i = 0; i < side; i++) {
             this.scene.pushMatrix();
-            this.scene.translate(4*i ,0, 0);
+            this.scene.translate(4*i + this.displacementsOnX[i],0, this.displacementsOnZ[i]);
             this.tree.display();
             this.scene.popMatrix();
         }
-            
-
     };
 }
