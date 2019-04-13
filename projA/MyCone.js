69d946acf44a5570a7f1d06999525b80249058f9
joao <jpabelha@hotmail.com>
2019-04-13 18:29:10 +0100

    added flag to cone

--- projA/MyCone.js
+++ projA/MyCone.js
@@ -9,16 +9,27 @@ class MyCone extends CGFobject {
         this.stacks = stacks;
         this.initBuffers();
     }
+
+    addBase() {
+        this.vertices.push(0, 0, 0);
+        this.texCoords.push(0.5, 0.5);
+        this.normals.push(0, 0, -1);
+
+        for(var i = 0; i <= this.slices; i++) {
+            this.indices.push((i + 1) % this.slices, i, this.slices + 1);
+        }
+    }
+
     initBuffers() {
         this.vertices = [];
         this.indices = [];
         this.normals = [];
         this.texCoords = [];
+        this.base = true;
 
         var ang = 0;
         var alphaAng = 2*Math.PI/this.slices;
 
-        
         for(var i = 0; i < this.slices; i++){
 
             this.vertices.push(Math.cos(ang), 0, -Math.sin(ang));
@@ -31,17 +42,14 @@ class MyCone extends CGFobject {
         this.texCoords.push(0,0,0);
         this.normals.push(0,1,0);
 
-        this.vertices.push(0, 0, 0);
-        this.texCoords.push(0.5, 0.5);
-        this.normals.push(0, 0, -1);
-
-        for(var i = 0; i <= this.slices; i++) {
-            this.indices.push((i + 1) % this.slices, i, this.slices + 1);
+        if(this.base) {
+            this.addBase();
         }
-
+        
         this.primitiveType = this.scene.gl.TRIANGLES;
         this.initGLBuffers();
     }
+
     updateBuffers(complexity){
         this.slices = 3 + Math.round(9 * complexity); //complexity varies 0-1, so slices varies 3-12
 
