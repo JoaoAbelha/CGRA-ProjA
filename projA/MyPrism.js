18c782f11427f97f9437d84b770eae03625d6e83
joao <jpabelha@hotmail.com>
2019-04-13 17:47:28 +0100

    flags for prism

--- projA/MyPrism.js
+++ projA/MyPrism.js
@@ -11,8 +11,41 @@ class MyPrism extends CGFobject {
     this.stacks = stacks;
     this.initBuffers();
   }
+  addBaseCoords() {
+    this.vertices.push(0, 0, 0);
+    this.texCoords.push(0.5, 0.5);
+    this.normals.push(0, 0 ,-1);
+  }
 
+  addTopCoords() {
+    this.vertices.push(0, 0, 1);  
+    this.texCoords.push(0.5, 0.5);
+    this.normals.push(0, 0 , 1);
+  }
+
+  drawBase() {
+    for (let i = 1; i < this.slices * 2; i++) {
+      this.indices.push(i + 1, i, 0);
+    }
+  }
+
+  drawTop(indexNumber) {
+     for (let i = 1; i < this.slices * 2; i++) {
+       this.indices.push(indexNumber + this.slices * 2 - i, indexNumber + this.slices * 2 + 1 - i, indexNumber + this.slices * 2 + 1);
+    }
+  }
+
+  addIndices(start,end ) {
+    for ( let i = start ; i < end; i += 2) {
+      this.indices.push(i, i + 1, i + 1 + this.slices * 2);
+      this.indices.push(i, i + 1 + this.slices * 2, i + this.slices * 2);
+    }
+  }
+  
   initBuffers() {
+
+    this.base = false;
+    this.top = true;
 
     this.vertices = [];
     this.indices = [];
@@ -22,9 +55,9 @@ class MyPrism extends CGFobject {
     let alphaAng = 2 * Math.PI / this.slices;
     let stack = 1 / this.stacks;
 
-    this.vertices.push(0, 0, 0);
-    this.texCoords.push(0.5, 0.5);
-    this.normals.push(0, 0 ,-1);
+    if (this.base || this.top) { //why top
+      this.addBaseCoords();
+    }
 
     for (let k = 0; k <= this.stacks; k++) {
       for (let i = 0; i < this.slices; i++) {
@@ -43,27 +76,38 @@ class MyPrism extends CGFobject {
       }
     }
 
-    this.vertices.push(0, 0, 1);  
-    this.texCoords.push(0.5, 0.5);
-    this.normals.push(0, 0 , 1);
+    if (this.top) {
+
+      this.addTopCoords();
+    }
 
     var indexNumber = 2 * this.slices * this.stacks;
 
-    for (let i = 1; i < this.slices * 2; i++) {
-       this.indices.push(indexNumber + this.slices * 2 - i, indexNumber + this.slices * 2 + 1 - i, indexNumber + this.slices * 2 + 1);
+    if (this.base && !this.top) {
+      this.drawBase();
+      this.addIndices(1, indexNumber);
+    } 
+    else if (!this.base && this.top) {
+      this.drawTop(indexNumber);
+      this.addIndices(1, indexNumber);
     }
-
-    for (let i = 1; i < this.slices * 2; i++) {
-      this.indices.push(i + 1, i, 0);
+    else if (this.base && this.top) {
+      this.drawBase();
+      this.drawTop(indexNumber);
+      this.addIndices(1, indexNumber + 1);
     }
-
-    for (let i = 1; i <= indexNumber; i += 2) {
-      this.indices.push(i, i + 1, i + 1 + this.slices * 2);
-      this.indices.push(i, i + 1 + this.slices * 2, i + this.slices * 2);
+    else {
+      this.addIndices(0, indexNumber);
     }
-
-
+    
     this.primitiveType = this.scene.gl.TRIANGLES;
     this.initGLBuffers();
   };
+
+
+
+
+
+
 };
+
